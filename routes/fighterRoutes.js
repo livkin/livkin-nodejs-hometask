a95a55414cdfdb4c4c94652e422b35e0d0579e82
livkin <gennadytabala@gmail.com>
2021-05-16 19:50:42 +0300

    fighter-api started

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -1,10 +1,36 @@
 const { Router } = require('express');
-const FighterService = require('../services/fighterService');
-const { responseMiddleware } = require('../middlewares/response.middleware');
+// const FighterService = require('../services/fighterService');
+// const { responseMiddleware } = require('../middlewares/response.middleware');
+const fighterManagerMiddleware = require('../middlewares/fighter.manager.middleware');
 const { createFighterValid, updateFighterValid } = require('../middlewares/fighter.validation.middleware');
 
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+
+// GET / api / users
+router.get('/',
+  userManagerMiddleware.getUsersAll  
+);
+// GET / api / users /: id
+router.get('/:id',
+  userManagerMiddleware.getUserById  
+);
+// POST / api / users
+router.post('/',
+  createUserValid,
+  userManagerMiddleware.createUser
+);
+
+// PUT / api / users /: id
+router.put('/:id',
+  updateUserValid,
+  userManagerMiddleware.updateUser
+);
+
+// DELETE / api / users /: id
+router.delete('/:id',
+  userManagerMiddleware.deleteUser
+);
 
 module.exports = router;
